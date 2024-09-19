1a474abf23c1cdca05a3e265198434a3c76a28bc
mohnoor94 <moh.noor94@gmail.com>
2024-09-19 17:22:01 +0300

    feat: add graphql operations as submodule

---
+++ scripts/graphql/update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd code/src/graphql-definitions || exit
+
+# Call it with a branch name to update the submodule to that branch, or with no arguments to update to the main branch.
+BRANCH=${1:-main}
+
+git checkout "$BRANCH"
+
+git pull origin "$BRANCH"
