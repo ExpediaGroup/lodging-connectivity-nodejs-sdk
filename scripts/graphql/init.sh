acd517d08352828a4ddceed589350e709c237bcc
Mohammad Noor Abu Khleif <moh.noor94@gmail.com>
2024-09-19 17:44:01 +0300

    feat: add operations repo as a submodule (#12)

---
+++ scripts/graphql/init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+git submodule init
+git submodule update --remote --merge
+
+cd .git/modules/code/src/graphql-definitions || exit
+
+mkdir info
+cd info || exit
+
+cat > sparse-checkout <<EOL
+/*
+!CHANGELOG.md
+!CONTRIBUTING.md
+!LICENSE
+!README.md
+!.github
+!.gitignore
+EOL
+
+git config core.sparseCheckout true
+
+cd "$(git rev-parse --show-toplevel)" || exit
+git read-tree -mu HEAD
+git pull origin "$(git rev-parse --abbrev-ref HEAD)"
